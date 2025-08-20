95e75ab9813c2b4285ab889e56b2085702e94d68
Davi Souza <davii.clink@gmail.com>
2025-08-20 16:58:05 -0300

    feat: correção de layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center px-10">
         <h1 className="text-3xl text-gray-900 font-bold my-7 text-center items-center">Seja bem vindo ao site oficial de pesquisa da tabela FIPE!</h1>
 
-        <section className="flex flex-col gap-30 lg:flex-row px-20 w-full justify-center items-center">
+        <section className="flex flex-col gap-30 mb-10 lg:flex-row px-20 w-full justify-center items-center">
           <CardDadosCarro />
           <CardFipe />
         </section>
